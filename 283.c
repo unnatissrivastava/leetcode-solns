0471ee48330d150d12be43418ed7c8561d89b5ad
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-26 11:52:21 +0530

    Update 283.c

--- 283.c
+++ 283.c
@@ -1,1 +1,13 @@
-
+void moveZeroes(int* nums, int numsSize) {
+    int nonzero = 0;
+    for (int i=0;i<numsSize; i ++){
+        if (nums[i]!=0){
+            nums[nonzero] = nums[i];
+            nonzero++;
+        }
+    }
+    for(int i = nonzero;i<numsSize;i++){
+        nums[i] = 0;
+    }
+    
+}
