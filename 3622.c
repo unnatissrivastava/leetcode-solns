44898e3f6cf1b1e55ffe1a63d6533d7e3e7f9e6f
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-26 12:37:40 +0530

    Update 3622.c

--- 3622.c
+++ 3622.c
@@ -1,1 +1,15 @@
+bool checkDivisibility(int n) {
+    int sum=0 ,product=1;
+    int a = n;
+    while(a!=0){
+        int b = a%10;
+        sum += b;
+        product *= b;
+        a /= 10;
 
+    }
+
+    return n% (sum+product) == 0;
+     
+        
+}
