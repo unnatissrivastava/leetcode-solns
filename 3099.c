b040d50bac018f4433060d9102283892d7a868c5
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-26 13:02:53 +0530

    Update 3099.c

--- 3099.c
+++ 3099.c
@@ -1,1 +1,12 @@
+int sumOfTheDigitsOfHarshadNumber(int x) {
+    int temp, digit, sum = 0;
 
+    temp = x;
+
+    while (temp > 0) {
+        digit = temp % 10;
+        sum = sum + digit;
+        temp = temp / 10;
+    }
+    return x%sum == 0?sum:-1;
+}
