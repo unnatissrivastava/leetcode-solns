9e71f489f15a9013470928b39dbf57017073a72d
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-26 23:41:07 +0530

    Update 20.cpp

--- 20.cpp
+++ 20.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> match = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
 
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            } else {
+                if (st.empty() || st.top() != match[c]) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
