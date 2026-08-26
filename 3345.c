1da709ea08a19fc08e7c34864c0753b3756b8f7d
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-26 12:52:01 +0530

    Update 3345.c

--- 3345.c
+++ 3345.c
@@ -1,1 +1,13 @@
-
+int pdigit(int n){
+    int p = 1;
+    while (n!= 0){
+        int a = n%10;
+        p *= a;
+        n/=10;
+    }
+    return p;
+}
+int smallestNumber(int n, int t) {
+    while(pdigit(n)%t != 0) n++;
+    return n;   
+}
